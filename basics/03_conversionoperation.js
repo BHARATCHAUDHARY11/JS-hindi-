67c1bca1d921f3c4f8f2201f62ba0c70790411b7
BHARAT CHAUDHARY <140901297+BHARATCHAUDHARY11@users.noreply.github.com>
2023-11-18 16:42:06 +0000

    conversion data types done

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -1,1 +1,26 @@
-let score = "Bharat
+let score = "Bharat"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33 abc"=> NaN
+// true => 1; false=> 0;
+
+let isloggedIn ="Bharat"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 =>true; 0=> false
+// ""=>false 
+// "bharat"=> true 
+
+let SomeNumber =33
+
+let stringNumber =>String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
